6eb1494a82bb8986bdfd05020209d6dcf933b703
niemi <nieminentommi@hotmail.com>
2022-09-16 13:46:25 +0300

    added 2017 plugin

--- TermInjectorPlus2017/Properties/AssemblyInfo.cs
+++ TermInjectorPlus2017/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c22ba65d-cfda-4e96-8dad-b7c425e26d26")]
+[assembly: Guid("26960B1D-890D-41A1-9EE2-26B505D4C4A5")]
 
 // Version information for an assembly consists of the following four values:
 //
